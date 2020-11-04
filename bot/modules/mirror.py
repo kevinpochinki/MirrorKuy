116536475ef0902bab875d96e259c97ade4e0bed
fauzanaditia7 <64374478+fauzanaditia7@users.noreply.github.com>
2020-11-04 20:06:26 +0700

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -136,15 +136,15 @@ class MirrorListener(listeners.MirrorListeners):
 
     def onUploadComplete(self, link: str):
         with download_dict_lock:
-            msg = f'<b>○ 🌀 Filename : </b><code>{download_dict[self.uid].name()}</code>\n\n<b>○ 💾 Total Size : </b><code>{download_dict[self.uid].size()}</code>'
+            msg = f'<b>○ 🖥️ Filename : </b><code>{download_dict[self.uid].name()}</code>\n\n<b>○ 💾 Total Size : </b><code>{download_dict[self.uid].size()}</code>'
             buttons = button_build.ButtonMaker()
-            buttons.buildbutton("🌍 𝗚-𝗗𝗥𝗜𝗩𝗘 𝗨𝗥𝗟", link)
+            buttons.buildbutton("🇮🇩 𝗚-𝗗𝗥𝗜𝗩𝗘 𝗨𝗥𝗟", link)
             LOGGER.info(f'Done Uploading {download_dict[self.uid].name()}')
             if INDEX_URL is not None:
                 share_url = requests.utils.requote_uri(f'{INDEX_URL}/{download_dict[self.uid].name()}')
                 if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
                     share_url += '/'
-                buttons.buildbutton("🏷️ 𝗜𝗡𝗗𝗘𝗫 𝗨𝗥𝗟 🏷️", share_url)
+                buttons.buildbutton("🔗 𝗜𝗡𝗗𝗘𝗫 𝗨𝗥𝗟 🔗", share_url)
             if BUTTON_THREE_NAME is not None and BUTTON_THREE_URL is not None:
                 buttons.buildbutton(f"{BUTTON_THREE_NAME}", f"{BUTTON_THREE_URL}")
             if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
@@ -156,7 +156,7 @@ class MirrorListener(listeners.MirrorListeners):
             else:
                 uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
             if uname is not None:
-                msg += f'\n\n<b>○👥 Uploader:</b>👉 {uname}\n\n🔹#Uploaded To Team Drive ✔️\n\n⛔ 𝗗𝗢 𝗡𝗢𝗧 𝗦𝗛𝗔𝗥𝗘 𝗧𝗛𝗘 𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞 𝗔𝗡𝗬𝗪𝗛𝗘𝗥𝗘 𝗘𝗟𝗦𝗘!\n\n<b>🛡️𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗕𝘆:: @MDH_HINDI</b>'
+                msg += f'\n\n<b>○📡 Uploader:</b>👉 {uname}\n\n🔹#TerUpload Bossku 😁 ✔️\n\n⛔ 𝗗𝗢 𝗡𝗢𝗧 𝗦𝗛𝗔𝗥𝗘 𝗧𝗛𝗘 𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞 𝗔𝗡𝗬𝗪𝗛𝗘𝗥𝗘 𝗘𝗟𝗦𝗘!\n\n</b>'
             try:
                 fs_utils.clean_download(download_dict[self.uid].path())
             except FileNotFoundError:
